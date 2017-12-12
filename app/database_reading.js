1827ecdbc69f4b44623f79d319a1a0eee594c371
ajayyy <ajay@ajayinkingston.com>
2017-12-12 16:30:11 -0500

    Shows amount of times a video was watched in the list

--- app/database_reading.js
+++ app/database_reading.js
@@ -49,7 +49,7 @@ var readDatabase = function(){
       document.getElementById("vidAmount").innerHTML = row.totalCount;
     });
 
-    db.each(`SELECT title, vid, author_id, COUNT(title) as totalCount
+    db.each(`SELECT vid, COUNT(title) as totalCount
             FROM videoshistory
             GROUP BY title
             ORDER BY totalCount DESC
@@ -60,7 +60,7 @@ var readDatabase = function(){
       print(row.title)
       document.getElementById("topvideo").innerHTML = row.title + " by " + row.author_id;
 
-      let callback = function(response, index){
+      let callback = function(response, index, row){
         var videos = response.items;
         if (videos.length == 0) {
           print('No video found.');
@@ -72,7 +72,7 @@ var readDatabase = function(){
       getVideoData({
           part: 'snippet',
           id: row.vid.replace("/watch?v=", "")
-      }, callback, 0);
+      }, callback, 0, row);
 
     });
 
@@ -85,13 +85,13 @@ var readDatabase = function(){
         print(err.message);
       }
 
-      let callback = function(response, index){
+      let callback = function(response, index, row){
         var videos = response.items;
         if (videos.length == 0) {
           print('No video found.');
         } else {
           print(videos[0].snippet.categoryId + " " + videos[0].snippet.title)
-          smallVideoList[index] = "<img style=\"margin-right: 10px;\" src=\"" + videos[0].snippet.thumbnails.default.url + "\"/>" + videos[0].snippet.title + " by " + videos[0].snippet.channelTitle + "<br/>";
+          smallVideoList[index] = "<img style=\"margin-right: 10px;\" src=\"" + videos[0].snippet.thumbnails.default.url + "\"/> <p style=\"display:inline-block;\">" + videos[0].snippet.title + " by " + videos[0].snippet.channelTitle + "<br/> Watched " + row.totalCount + " times</p> <br/>";
 
           smallVideoListLoadedAmount++;
 
@@ -110,7 +110,7 @@ var readDatabase = function(){
       getVideoData({
           part: 'snippet',
           id: row.vid.replace("/watch?v=", "")
-      }, callback, smallVideoListIndex);
+      }, callback, smallVideoListIndex, row);
 
       smallVideoListIndex++;
 
@@ -128,7 +128,7 @@ var readDatabase = function(){
       document.getElementById("topchannel").innerHTML = row.author_id;
       row.author_id = "/user/enyay";
 
-      let callback = function(response, index){
+      let callback = function(response, index, row){
         var channel = response.items;
         if (channel.length == 0) {
           print('No channel found.');
@@ -146,7 +146,7 @@ var readDatabase = function(){
         getChannelData({
             part: 'snippet',
             id: row.author_id.replace("/channel/", "")
-        }, callback, 0);
+        }, callback, 0, row);
       }
 
     });
@@ -163,7 +163,7 @@ var readDatabase = function(){
       document.getElementById("topchannel").innerHTML = row.author_id;
       // row.author_id = "/user/enyay";
 
-      let callback = function(response, index){
+      let callback = function(response, index, row){
         var channel = response.items;
         if (channel.length == 0) {
           print('No channel found.');
@@ -190,12 +190,12 @@ var readDatabase = function(){
         getChannelData({
             part: 'snippet',
             forUsername: row.author_id.replace("/user/", "")
-        }, callback, smallChannelListIndex);
+        }, callback, smallChannelListIndex, row);
       }else{ //must be channel id
         getChannelData({
             part: 'snippet',
             id: row.author_id.replace("/channel/", "")
-        }, callback, smallChannelListIndex);
+        }, callback, smallChannelListIndex, row);
       }
 
       smallChannelListIndex++;
@@ -249,24 +249,24 @@ function excludeMusic(){
   });
 }
 
-function getChannelData(params, callback, index){
+function getChannelData(params, callback, index, row){
 
   youtube.channels.list(params, function(err, response) {
     if (err) {
       print('The API returned an error: ' + err);
       return;
     }
-    callback(response, index)
+    callback(response, index, row)
   });
 }
 
-function getVideoData(params, callback, index){
+function getVideoData(params, callback, index, row){
 
   youtube.videos.list(params, function(err, response) {
     if (err) {
       print('The API returned an error: ' + err);
       return;
     }
-    callback(response, index)
+    callback(response, index, row)
   });
 }
