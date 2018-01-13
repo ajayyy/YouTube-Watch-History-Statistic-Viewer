9fe27383d6892e22043736e5e5adc9c7428ae804
= <ajay@ajayinkingston.com>
2018-01-13 00:05:00 -0500

    Fixed issue with process not ending when the close button was pressed

--- app/main.js
+++ app/main.js
@@ -60,6 +60,12 @@ function newScrapeMessage(data) {
 
 }
 
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+      app.quit();
+    }
+});
+
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
         height: 600,
